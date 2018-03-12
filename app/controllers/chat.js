7985f911a8f562e69e976e4d0b162a216b4ff1f2
Wallison do Carmo Costa <wallisoncarmo01@gmail.com>
2018-03-12 01:04:56 -0300

    [Add] Validacao de apelido na tela inicial

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,16 @@
 module.exports.iniciaChat = function(application, req,res){
+
+	var form = req.body;
+
+	req.assert('apelido','Nome ou apelido é obrigatório!').notEmpty();
+	req.assert('apelido','Nome ou apelido deve conter de 3 a 5 caracter!').len(3,15);
+
+	var erros = req.validationErrors();
+
+	if(erros){
+		res.render('index',{validacao : erros});
+		return;
+	}
+
 	res.render('chat');
 }
