ca8202542c521ce654c52809f6dbdf28a0274d55
Wallison do Carmo Costa <wallisoncarmo01@gmail.com>
2018-03-25 20:45:40 -0300

    Finalizacao do projeto

--- app.js
+++ app.js
@@ -1,39 +1,65 @@
 /* Importação das configurações do servidor */
-var  app = require('./config/server');
+var app = require('./config/server');
 
 /* Parametrização para a porta de escuta */
-var server = app.listen(80, function (){
+var server = app.listen(80, function () {
 	console.log('Servidor OK');
 });
 
 /* chama do websocket */
 var io = require('socket.io').listen(server);
 
-app.set('io',io);
+app.set('io', io);
 
 /* criar conexão para o websocket */
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 	console.log('Usuário conectou');
 
-	socket.on('disconnect', function(socket){
+	socket.on('disconnect', function (socket) {
 		console.log('Usuário desconectou');
 	});
 
-	socket.on('disconnect', function(socket){
+	socket.on('disconnect', function (socket) {
 		console.log('Usuário desconectou');
 	});
 
-	socket.on('msgParaServidor', function(data){
+	socket.on('msgParaServidor', function (data) {
+
+		/* dialogo */
 		socket.emit(
-			'msgParaCliente', 
-			{apelido:data.apelido,
-				mensagem:data.mensagem});
+			'msgParaCliente',
+			{
+				apelido: data.apelido,
+				mensagem: data.mensagem
+			}
+		);
 
 		socket.broadcast.emit(
-			'msgParaCliente', 
-			{apelido:data.apelido,
-				mensagem:data.mensagem});
+			'msgParaCliente',
+			{
+				apelido: data.apelido,
+				mensagem: data.mensagem
+			}
+		);
 
+
+		/* atualizar os participantes */
+		if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+			socket.emit(
+				'participantesParaCliente',
+				{
+					apelido: data.apelido
+				}
+			);
+			
+			socket.broadcast.emit(
+				'participantesParaCliente',
+				{
+					apelido: data.apelido
+				}
+			);		
+
+		}
 
 	});
 
